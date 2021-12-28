088bd304c58b7138c1e59742494d68fca33f4b0e
Jordi <jordi@naddesign.ca>
2021-12-28 09:42:27 -0500

    Added multiple subprocess.

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import os
+from multiprocessing import Process
 from dotenv import load_dotenv
-
+import os
+from src.voice_recognition import voice_recognition
 from src.live_faces_recognition import live_faces_recognition
+
+from utils import utils
 
 load_dotenv(dotenv_path="config/.env")
 
-ENCODE_FACES = os.getenv('ENCODE_FACES')
+DATA_AUGMENTATION = os.getenv('DATA_AUGMENTATION')
 
-def main():
-    if ENCODE_FACES == "true":
-        print("encoding")
-        live_faces_recognition.encodeFaces()
-    live_faces_recognition.liveRecognition()
+def live_capture():
+
+    if DATA_AUGMENTATION == "True":
+        utils.data_augmentation()
+    
+    live_faces_recognition.video_capture()
+
+def speech_rec():
+    voice_recognition.speechRecognition()
+
 
 if __name__ == "__main__":
-    main()
+    p1 = Process(target=live_capture, args=())
+    p2 = Process(target=speech_rec, args=())
+
+    processes = list()
+    processes.append(p1)
+    processes.append(p2)
+
+    for p in processes:
+        p.start()
