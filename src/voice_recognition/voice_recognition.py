ac6df9f43db098cdbd5bf056b86ea54d4a728902
Jordi <jordi@naddesign.ca>
2022-05-21 11:59:28 -0400

    Moved script in function.

--- src/voice_recognition/voice_recognition.py
+++ src/voice_recognition/voice_recognition.py
@@ -1,18 +1,19 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
-
-f = open("data/transcription.txt", "w")
-
-mic = sr.Microphone()
-
-if not isinstance(r, sr.Recognizer):
-    raise TypeError("`recognizer` must be `Recognizer` instance")
-
-if not isinstance(mic, sr.Microphone):
-    raise TypeError("`microphone` must be `Microphone` instance")
-
 def speechRecognition():
+        
+    r = sr.Recognizer()
+
+    f = open("data/transcription.txt", "w")
+
+    mic = sr.Microphone()
+
+    if not isinstance(r, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(mic, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
     print("listening")
     with mic as source:
         r.adjust_for_ambient_noise(source)
