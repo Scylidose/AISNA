6aea845e26fa031363c1fd3fc08a49d74b5d9ee4
Jordi <jordi@naddesign.ca>
2022-05-23 10:36:39 -0400

    Debugged unkillable process.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process
 from dotenv import load_dotenv
 import os
+import sys
 from src.voice_recognition import voice_recognition
 from src.live_faces_recognition import live_faces_recognition
 
@@ -20,8 +21,7 @@ def live_capture():
 def speech_rec():
     voice_recognition.speechRecognition()
 
-
-if __name__ == "__main__":
+def main():
     p1 = Process(target=live_capture, args=())
     p2 = Process(target=speech_rec, args=())
 
@@ -31,3 +31,12 @@ if __name__ == "__main__":
 
     for p in processes:
         p.start()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
